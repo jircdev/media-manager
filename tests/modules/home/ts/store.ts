c44e5f1d3deb362b45a167095d9a6fc26df6c68f
Julio Rodriguez <jircdev@gmail.com>
2023-10-18 20:03:45 -0300

    feat: Add ui to upload elements

--- tests/modules/home/ts/store.ts
+++ tests/modules/home/ts/store.ts
@@ -1,2 +1,19 @@
 import type { IWidgetStore } from '@beyond-js/widgets/controller';
-export class StoreManager implements IWidgetStore {}
+import { Uploader } from '@bgroup/media-manager/uploader';
+export class StoreManager implements IWidgetStore {
+	#uploader: Uploader;
+	get uploader() {
+		return this.#uploader;
+	}
+	constructor() {
+		this.#uploader = new Uploader({
+			name: 'files',
+			url: '/api/upload',
+			multiple: true,
+		});
+	}
+
+	create(input: HTMLElement, draggable: HTMLElement) {
+		this.#uploader.create(input, draggable);
+	}
+}
--- tests/modules/home/ts/views/index.tsx
+++ tests/modules/home/ts/views/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
-
+import { useBinder } from '@beyond-js/react-18-widgets/hooks';
+import { Images } from './images';
 export /*bundle*/
-function View(): JSX.Element {
+function View({ store }): JSX.Element {
 	const ref = React.useRef<HTMLCanvasElement>(null);
+	const draggable = React.useRef<HTMLCanvasElement>(null);
+
+	const [items, setItems] = React.useState([]);
+	React.useEffect(() => {
+		store.create(ref.current, draggable.current);
+	}, []);
+
+	useBinder([store.uploader], () => {
+		console.log(10, store.uploader.files?.items);
+		setItems(store.uploader.files?.items);
+	});
+
 	return (
 		<div className='page__container'>
-			<h1>
-				<canvas ref={ref} />
-			</h1>
+			<h1>Hello World!</h1>
+			<section className='box__container' ref={draggable}>
+				<button ref={ref}>Cargar Elementos</button>
+			</section>
+			<hr />
+			<button>Enviar archivos</button>
+			<section>
+				<Images items={items} />
+			</section>
 		</div>
 	);
 }
